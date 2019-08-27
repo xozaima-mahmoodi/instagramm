db8e0d04a375c1848814ac30f3f9646ce213b69a
Your Name <you@example.com>
2019-08-27 11:09:34 +0430

    Show and Index Pages

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,13 @@
 class PicsController < ApplicationController
+    before_action :find_pic, only: [:show, :edit, :update, :destroy]
+
 
     def index
         
+    end
+
+    def show
+      
     end
 
     def new
@@ -10,11 +16,23 @@ class PicsController < ApplicationController
     
     def create
       @pic = Pic.new(pic_params)
+
+      if @pic.save
+        redirect_to @pic, notice: "Yesss! It was posted!"
+      else
+        render 'new'
+      end
     end
 
+
     private
+
     def pic_params
       params.require(:pic).permit(:title, :description)
+    end
+
+    def find_pic
+      @pic = Pic.find(params[:id])
     end
 
 end
